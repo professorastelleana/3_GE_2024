978cc3978cc25bc839a1a6b7cb0d579d9f1ea153
professorastelleana <145031308+professorastelleana@users.noreply.github.com>
2024-11-11 20:36:26 -0300

    ajustes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function criaCartao(categoria, pergunta, resposta){
                 </div>
             </div>  
        `
-
+       let respostaEstaVisivel = false
        function viraCartao() {
             respostaEstaVisivel = !respostaEstaVisivel
             cartao.classList.toggle('active', respostaEstaVisivel)
